e7a4ebe220d8ff3c2ab2babe70de7092ad762041
sundonghwan <sunehd564@gmail.com>
2022-02-08 08:17:29 +0900

    pytorch rnn 이름으로 나라 분류 실습

--- pytorch/rnn/classification.py
+++ pytorch/rnn/classification.py
@@ -9,14 +9,13 @@ import os
 
 def findFiles(path): return glob.glob(path)
 
-print(findFiles('./data/names/*.txt'))
-
 import unicodedata
 import string
 
 all_letters = string.ascii_letters + ".,;'"
 n_letters = len(all_letters)
-
+print(n_letters)
+print(all_letters)
 
 def unicodeToAscii(s):
     return ''.join(
@@ -25,4 +24,217 @@ def unicodeToAscii(s):
         and c in all_letters
     )
 
-print(unicodeToAscii('Ślusàrski'))
+category_lines = {}
+all_categories = []
+
+def readLines(filename):
+    with open(filename, 'r', encoding='utf8') as f:
+        lines = f.read().strip().split('\n')
+        return [unicodeToAscii(line) for line in lines]
+
+data = readLines('./data/names\\Arabic.txt')
+
+for filename in findFiles('./data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+n_categories = len(all_categories)
+
+
+
+'''
+.. NOTE::
+역자 주:  One-Hot 벡터는 언어를 다룰 때 자주 이용되며,
+단어,글자 등을 벡터로 표현 할 때 단어,글자 사이의 상관 관계를 미리 알 수 없을 경우,
+One-Hot으로 표현하여 서로 직교한다고 가정하고 학습을 시작합니다.
+동일하게 상관 관계를 알 수 없는 다른 데이터의 경우에도 One-Hot 벡터를 활용 할 수 있습니다.
+'''
+
+import torch
+
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+'''
+print(letterToTensor('J'))
+print(lineToTensor('Jones').size())
+'''
+from model import RNN
+
+n_hidden = 128
+rnn = RNN(n_letters, n_hidden, n_categories)
+
+input = lineToTensor('Albert')
+hidden = torch.zeros(1, n_hidden)
+print(input[0])
+output, next_hidden = rnn(input[0], hidden)
+print(output)
+
+# 우리가 알아낸 각 카테고리의 우도인 네트워크 출력을 해석하는 것  가장 큰 값의 주소를 알기 위해서 Tensor.topk 를 사용 할 수 있습니다
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1) # 텐서의 가장 큰 값 및 주소
+    category_i = top_i[0].item()     # 텐서에서 정수 값으로 변경
+    return all_categories[category_i], category_i
+
+print(categoryFromOutput(output))
+
+import random
+
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = lineToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+for i in range(10):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    print('category =', category, '/ line =', line)
+
+import torch.nn as nn
+'''
+
+    CrossEntropyLoss와 NLLLoss둘다 분류에서 사용됨
+    모델의 마지막 레이어에 Softmax가 있는 경우 NLLLoss 를 활용 없을시 Cross entropyloss를 활용
+
+'''
+criterion = nn.NLLLoss()
+learning_rate = 0.005
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+    # gradients값들을 추후에 backward를 해줄때 계속 더해주기 때문에 gradients를 zero로 만들어주고 시작
+    rnn.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+
+    return output, loss.item()
+
+import time
+import math
+
+n_iters = 150000
+print_every = 5000
+plot_every = 1000
+
+# 도식화를 위한 손실 추적
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # iter 숫자, 손실, 이름, 추측 화면 출력
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+
+    # 현재 평균 손실을 전체 손실 리스트에 추가
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+
+# 혼란 행렬에서 정확한 추측을 추적
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+plt.figure()
+plt.plot(all_losses)
+# 혼란 행렬에서 정확한 추측을 추적
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+
+# 주어진 라인의 출력 반환
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+
+# 예시들 중에 어떤 것이 정확하게 예측되었는지 기록
+for i in range(n_confusion):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output = evaluate(line_tensor)
+    guess, guess_i = categoryFromOutput(output)
+    category_i = all_categories.index(category)
+    confusion[category_i][guess_i] += 1
+
+# 모든 행을 합계로 나누어 정규화
+for i in range(n_categories):
+    confusion[i] = confusion[i] / confusion[i].sum()
+
+# 도식 설정
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+# 축 설정
+ax.set_xticklabels([''] + all_categories, rotation=90)
+ax.set_yticklabels([''] + all_categories)
+
+# 모든 tick에서 레이블 지정
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+# sphinx_gallery_thumbnail_number = 2
+plt.show()
+
+def predict(input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    with torch.no_grad():
+        output = evaluate(lineToTensor(input_line))
+
+        # Get top N categories
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_index = topi[0][i].item()
+            print('(%.2f) %s' % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+predict('Dovesky')
+predict('Jackson')
+predict('Satoshi')
